51bd9dcb49a91283360d02b4f96ee5c9e11fbb62
VanshRuhela09 <ruhelavansh170@gmail.com>
2025-08-22 19:26:44 +0530

    add Elearning_frontend

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./footer.css";
+import {
+  AiFillFacebook,
+  AiFillTwitterSquare,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>
+          &copy; E-Learning Platform. All rights reserved.
+        </p>
+        <div className="social-links">
+          
+          <a href="">
+            <AiFillInstagram />
+          </a>
+
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
